0e1eceee04db8a9b52ace7c0f0aa33183b027cf1
thosoo <mail@thaison-nguyen.de>
2025-02-14 10:35:36 +0100

    Separate kernel and patch version in patch.sh

--- contrib/dkms/patch.sh
+++ contrib/dkms/patch.sh
@@ -1,16 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
-VERSION=${1:-5.8}
+# If $1 is not provided, KERNEL_VERSION defaults to 6.8
+KERNEL_VERSION=${1:-6.8}
 
-echo "Patching for kernel ${VERSION}"
+# If $2 is not provided, VERSION defaults to 5.18
+PATCH_VERSION=${2:-5.18}
 
-wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa.h?h=linux-${VERSION}.y" -O hpsa.h
-wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa.c?h=linux-${VERSION}.y" -O hpsa.c
-wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa_cmd.h?h=linux-${VERSION}.y" -O hpsa_cmd.h
+echo "Patching for kernel ${KERNEL_VERSION} using Patch version ${PATCH_VERSION}"
 
+# Fetch the three driver files for the specified kernel version
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa.h?h=linux-${KERNEL_VERSION}.y" \
+     -O hpsa.h
+
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa.c?h=linux-${KERNEL_VERSION}.y" \
+     -O hpsa.c
+
+wget "https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/plain/drivers/scsi/hpsa_cmd.h?h=linux-${KERNEL_VERSION}.y" \
+     -O hpsa_cmd.h
+
+# Enable nullglob so the loop won't run if no patches match
 shopt -s nullglob
-for PATCH in ../../kernel/"${VERSION}"*/*.patch; do
+
+# Apply all patches that match ../../kernel/<VERSION>*/.patch
+for PATCH in ../../kernel/"${PATCH_VERSION}"*/*.patch; do
     echo "Applying ${PATCH}"
     patch --no-backup-if-mismatch -Np3 < "${PATCH}"
 done
